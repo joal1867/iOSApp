f4c354268cf6e8e1aa3141b46845df90c385fc7e
joal1867 <jjoal1867@gmail.com>
2018-11-26 10:42:20 +0900

    Initial Commit

---
+++ iOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSApp
+//
+//  Created by Joal on 26/11/2018.
+//  Copyright © 2018 Joal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
