76922289938a4fe12813a5150b6c5961704b3e4e
joal1867 <jjoal1867@gmail.com>
2018-11-30 17:26:33 +0900

    Side menu Bar

--- iOSApp/SideMenuTVC.swift
+++ iOSApp/SideMenuTVC.swift
@@ -2,82 +2,67 @@
 import UIKit
 
 class SideMenuTVC: UITableViewController {
+    //메뉴로 사용할 6개 항목과 이미지 저장
+    let titles = ["메모작성","친구 새글","달력보기","공지사항","통계","계정관리"]
+    let icons = [UIImage(named:"icon01.png"),UIImage(named:"icon02.png"),UIImage(named:"icon03.png")
+        ,UIImage(named:"icon04.png"),UIImage(named:"icon05.png"),UIImage(named:"icon06.png"),]
+    
+    //디자인 레이블2개, 이미지뷰1개를 인스턴스 변수로 생성
+    let nameLabel = UILabel()
+    let emailLabel = UILabel()
+    let profileImage = UIImageView()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        let headerView = UIView(frame: CGRect(x: 0, y: 0, width: self.view.frame.width, height: 70))
+        headerView.backgroundColor = UIColor.brown
+        self.tableView.tableHeaderView = headerView
+        
+        nameLabel.frame = CGRect(x: 70, y: 15, width: 100, height: 30)
+        nameLabel.text = "JOAL"
+        nameLabel.textColor = UIColor.white
+        self.nameLabel.backgroundColor = UIColor.clear
+        headerView.addSubview(nameLabel)
+        
+        emailLabel.frame = CGRect(x: 70, y: 30, width: 100, height: 30)
+        emailLabel.text = "jjoal1867@gmail.com"
+        emailLabel.textColor = UIColor.white
+        self.emailLabel.backgroundColor = UIColor.clear
+        headerView.addSubview(emailLabel)
+        
+        let defaultProfile = UIImage(named:"account.jpg")
+        profileImage.image = defaultProfile
+        profileImage.frame = CGRect(x:10,y:10,width:50,height:50)
+        //네모난 이미지 뷰를 등글게 만들기
+        profileImage.layer.cornerRadius = (profileImage.frame.width/2)
+        profileImage.layer.borderWidth = 0
+        profileImage.layer.masksToBounds = true
+        headerView.addSubview(profileImage)
     }
 
     // MARK: - Table view data source
-
+    //섹션의 개수를 설정하는 메소드
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
+    //섹션별 행의 개수를 설정하는 메소드
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return self.titles.count
     }
 
-    /*
+    //셀을 만들어 주는 메소드
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
+        
+        let id = "MenuCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: id) ?? UITableViewCell(style: .default, reuseIdentifier: id)
+       
+        cell.textLabel?.text = self.titles[indexPath.row]
+        cell.imageView?.image = self.icons[indexPath.row]
+        
 
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
